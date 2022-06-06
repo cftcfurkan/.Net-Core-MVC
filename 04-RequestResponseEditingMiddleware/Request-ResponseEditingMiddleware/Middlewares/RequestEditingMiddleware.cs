ee69b2c4f8e7f40a6294d65aea2d9b97469ce221
cftcfurkan <90473630+cftcfurkan@users.noreply.github.com>
2022-06-06 08:59:46 +0300

    Add files via upload

---
+++ 04-RequestResponseEditingMiddleware/Request-ResponseEditingMiddleware/Middlewares/RequestEditingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace RequestResponseEditingMiddleware.Middlewares
+{
+     public class RequestEditingMiddleware
+    {
+        private RequestDelegate _requestDelegate;
+        public RequestEditingMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            //ysk.com.tr/yavuz
+            if (context.Request.Path.ToString() == "/yavuz")
+            /*await _requestDelegate.Invoke(context);*/  // istek middleware e geldi bu isteği sonraki middleware e gönderen yapı invoke.
+            context.Response.WriteAsync("Hosgeldin yavuz");
+            else
+            {
+                await _requestDelegate.Invoke(context);
+            }
+        }
+    }
+}
---
+++ 04-RequestResponseEditingMiddleware/Request-ResponseEditingMiddleware/Middlewares/ResponseEditingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Request_ResponseEditingMiddleware.Middlewares
+{
+     public class ResponseEditingMiddleware
+    {
+        private RequestDelegate _requestDelegate;
+        public ResponseEditingMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _requestDelegate.Invoke(context);
+            if (context.Response.StatusCode == StatusCodes.Status404NotFound)
+            {
+                await context.Response.WriteAsync("Sayfa Bulunamadı!!!");
+            }
+        }
+    }
+}
