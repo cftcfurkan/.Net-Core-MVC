ebcb16be9f13a50c23faf7f30d6031b0fa18e208
cftcfurkan <90473630+cftcfurkan@users.noreply.github.com>
2022-06-06 08:58:43 +0300

    Add files via upload

---
+++ 02-Sync-Async/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace _02_Sync_Async
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btntopla = new System.Windows.Forms.Button();
+            this.btnCarp = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btntopla
+            // 
+            this.btntopla.Location = new System.Drawing.Point(181, 176);
+            this.btntopla.Name = "btntopla";
+            this.btntopla.Size = new System.Drawing.Size(110, 34);
+            this.btntopla.TabIndex = 0;
+            this.btntopla.Text = "Topla";
+            this.btntopla.UseVisualStyleBackColor = true;
+            this.btntopla.Click += new System.EventHandler(this.btntopla_Click);
+            // 
+            // btnCarp
+            // 
+            this.btnCarp.Location = new System.Drawing.Point(454, 176);
+            this.btnCarp.Name = "btnCarp";
+            this.btnCarp.Size = new System.Drawing.Size(97, 34);
+            this.btnCarp.TabIndex = 1;
+            this.btnCarp.Text = "Çarp";
+            this.btnCarp.UseVisualStyleBackColor = true;
+            this.btnCarp.Click += new System.EventHandler(this.btnCarp_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(181, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(454, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCarp);
+            this.Controls.Add(this.btntopla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btntopla;
+        private System.Windows.Forms.Button btnCarp;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ 02-Sync-Async/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_Sync_Async
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btntopla_Click(object sender, EventArgs e)
+        {
+            Topla(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+
+
+        }
+
+        private async void btnCarp_Click(object sender, EventArgs e)
+        {
+           //await CarpAsync(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            MessageBox.Show("İşlem Tamamlandı.");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Topla(int sayi1,int sayi2)
+        {
+            MessageBox.Show($"Toplam : {sayi1+sayi2}");
+        }
+        private void CarpAsync(int sayi1, int sayi2)
+        {
+            MessageBox.Show($"Çarpım : {sayi1*sayi2}");
+        }
+
+        private Task CarpmAsync(int sayi1, int sayi2) => Task.Run(() =>
+                                                                   {
+                                                                       Thread.Sleep(1000);
+                                                                       MessageBox.Show("Çarpım = "+sayi1*sayi2);
+                                                                   });
+
+    }
+}
