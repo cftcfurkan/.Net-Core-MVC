e71506f1415b323b3b0a3c59602a04980e5c7af0
cftcfurkan <90473630+cftcfurkan@users.noreply.github.com>
2022-06-06 08:58:20 +0300

    Add files via upload

---
+++ 01-Delege/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_Delege
+{
+    delegate void BenimDelegem (int sayi1,int sayi2);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenimDelegem benimDelegem = new BenimDelegem(Topla);  //geriye void dönen 2 int parametresi alan fonksiyon yapmamız lazım , 
+            benimDelegem += Carp;
+            benimDelegem.Invoke(1, 2);
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Topla (int numara1,int numara2)
+        {
+            Console.WriteLine(numara1+numara2);
+        }
+        static void Carp(int sayi1, int sayi2) // delegeler sırasıyla  metodları tetikleyen yapılardır.
+        {
+            Console.WriteLine(sayi1*sayi2);
+        }
+    }
+}
